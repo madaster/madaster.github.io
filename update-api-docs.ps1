2dfd13dadaebd619edefaa139c53a97ec13f5a39
Pieter-Paul Luijten <ppluijten@gmail.com>
2019-04-01 16:53:53 +0200

    Updated API docs to version 2.1.

--- update-api-docs.ps1
+++ update-api-docs.ps1
@@ -1,2 +1,3 @@
-Invoke-WebRequest https://api.madaster.com/swagger/v2.0/swagger.json -OutFile swagger.json
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest https://api.madaster.com/swagger/v2.1/swagger.json -OutFile swagger.json
 spectacle -t api-docs -l assets/images/madaster-logo.svg swagger.json
